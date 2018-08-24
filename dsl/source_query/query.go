6ed239e5ece121e80bf31e977844f1f351336fd5
sohanvir <sohanvir.singh@verse.in>
2018-08-24 13:54:31 +0530

    source Query Definition(DSL)- Review Comments

--- dsl/source_query/query.go
+++ dsl/source_query/query.go
@@ -3,14 +3,72 @@ package query
 import (
 	"fmt"
 	"github.com/dailyhunt/feed/util"
+	"strings"
 )
 
-type QueueQuery map[string]int
+const (
+	QGenre     QueueType = "GENRE"
+	QBreaking            = "BREAKING"
+	QMostLiked           = "MOST_LIKED"
+	QNPInbox             = "NP_INBOX"
+	QInvalid             = "INVALID"
 
-func (q QueueQuery) Add(name string, count int) QueueQuery {
-	if _, ok := q[name]; !ok {
-		q[name] = count
+	// Filter Type
+	OR  FilterType = "OR"
+	AND            = "AND"
+	NOT            = "NOT"
+
+	// Field Type
+	GENRE        FieldType = "GENRE"
+	IG                     = "IG"
+	CONTENT_TYPE           = "CONTENT_TYPE"
+	NSFW                   = "NSFW"
+)
+
+type FilterType string
+
+type Field string
+type FieldType string
+
+type QueueType string
+type QueueCount map[string]int
+type QueueQuery map[QueueType]QueueCount
+
+type SourceQueryFilter interface {
+	Type() FilterType
+	Fields() []Field
+}
+
+type filter struct {
+	fType  FilterType
+	fields []Field
+}
+
+type SourceQuery struct {
+	queueQueries QueueQuery
+	language     util.Language
+	orFilters    map[FieldType][]SourceQueryFilter
+	andFilters   map[FieldType][]SourceQueryFilter
+	notFilters   map[FieldType][]SourceQueryFilter
+}
+
+type SourceQueryBuilder struct {
+	queueQueries QueueQuery
+	language     util.Language
+	orFilters    map[FieldType][]SourceQueryFilter
+	andFilters   map[FieldType][]SourceQueryFilter
+	notFilters   map[FieldType][]SourceQueryFilter
+}
+
+func (q QueueQuery) Add(qType QueueType, name string, count int) QueueQuery {
+	if _, ok := q[qType]; !ok {
+		qCount := QueueCount{}
+		q[qType] = qCount
 	}
+
+	queueCount := q[qType]
+	queueCount[name] = count
+
 	return q
 }
 
@@ -18,24 +76,62 @@ func (q QueueQuery) Count() int {
 	return len(q)
 }
 
+func NewQueueQueryFromMap(qCountConfig map[string]map[string]int) QueueQuery {
+
+	query := QueueQuery{}
+
+	for qType, qConf := range qCountConfig {
+		queueType := GetQueTypeFromString(qType)
+		if queueType == QInvalid {
+			panic(fmt.Sprintf("invalid queue type passed %s", qType))
+		}
+
+		for qName, count := range qConf {
+			query.Add(queueType, qName, count)
+		}
+	}
+
+	return query
+}
+
 func NewQueueQuery() *QueueQuery {
 	return &QueueQuery{}
 }
 
-type SourceQuery struct {
-	queueQueries QueueQuery
-	language     util.Language
-	orFilters    []SourceQueryFilter
-	andFilters   []SourceQueryFilter
-	notFilters   []SourceQueryFilter
+func GetQueTypeFromString(qType string) QueueType {
+	types := map[string]QueueType{
+		"GENRE":      QGenre,
+		"BREAKING":   QBreaking,
+		"MOST_LIKED": QMostLiked,
+		"NP_INBOX":   QNPInbox,
+	}
+
+	upper := strings.ToUpper(qType)
+	if t, ok := types[upper]; ok {
+		return t
+	}
+
+	return QInvalid
 }
 
-type SourceQueryBuilder struct {
-	queueQueries QueueQuery
-	language     util.Language
-	orFilters    []SourceQueryFilter
-	andFilters   []SourceQueryFilter
-	notFilters   []SourceQueryFilter
+func (f *filter) Type() FilterType {
+	return f.fType
+}
+
+func (f *filter) Fields() []Field {
+	return f.fields
+}
+
+func MatchAny(fields ...Field) SourceQueryFilter {
+	return &filter{OR, fields}
+}
+
+func MatchAll(fields ...Field) SourceQueryFilter {
+	return &filter{AND, fields}
+}
+
+func MatchNone(fields ...Field) SourceQueryFilter {
+	return &filter{NOT, fields}
 }
 
 func (b *SourceQueryBuilder) Select(queueQueries QueueQuery) *SourceQueryBuilder {
@@ -53,20 +149,30 @@ func (b *SourceQueryBuilder) From(language util.Language) *SourceQueryBuilder {
 	return b
 }
 
-func (b *SourceQueryBuilder) Filter(filter SourceQueryFilter) *SourceQueryBuilder {
-	switch fType := filter.Type(); fType {
+func (b *SourceQueryBuilder) Filter(fieldType FieldType, queryFilter SourceQueryFilter) *SourceQueryBuilder {
+	switch fType := queryFilter.Type(); fType {
 	case OR:
-		fmt.Println("Add to OR Filter list")
+		b.addFilter(b.orFilters, fieldType, queryFilter)
 	case AND:
-		fmt.Println("Add to AND Filter list")
+		b.addFilter(b.andFilters, fieldType, queryFilter)
 	case NOT:
-		fmt.Println("Add to NOT Filter list")
+		b.addFilter(b.notFilters, fieldType, queryFilter)
 	default:
 		fmt.Println("Invalid Filter list and Panic ")
 	}
 
 	return b
 }
+
+func (b *SourceQueryBuilder) addFilter(queryFilters map[FieldType][]SourceQueryFilter, fieldType FieldType, queryFilter SourceQueryFilter) {
+	if filters, ok := queryFilters[fieldType]; ok {
+		queryFilters[fieldType] = append(filters, queryFilter)
+	} else {
+		var filters []SourceQueryFilter
+		queryFilters[fieldType] = append(filters, queryFilter)
+	}
+}
+
 func (b *SourceQueryBuilder) Build() *SourceQuery {
 	return &SourceQuery{
 		queueQueries: b.queueQueries,
@@ -75,9 +181,12 @@ func (b *SourceQueryBuilder) Build() *SourceQuery {
 		andFilters:   b.andFilters,
 		notFilters:   b.notFilters,
 	}
-
 }
 
 func NewSourceQueryBuilder() *SourceQueryBuilder {
-	return &SourceQueryBuilder{}
+	return &SourceQueryBuilder{
+		orFilters:  make(map[FieldType][]SourceQueryFilter),
+		andFilters: make(map[FieldType][]SourceQueryFilter),
+		notFilters: make(map[FieldType][]SourceQueryFilter),
+	}
 }
--- dsl/source_query/query_test.go
+++ dsl/source_query/query_test.go
@@ -6,30 +6,64 @@ import (
 	"testing"
 )
 
-func TestNewQueueQuery(t *testing.T) {
-	queueQuery := NewQueueQuery().
-		Add("breaking", 10).
-		Add("genre1", 20)
+// query from config map
+var qCountConfig = map[string]map[string]int{
+	"breaking": {
+		"breaking": 10},
+	"genre": {
+		"genre-1": 20,
+		"genre-2": 5},
+}
 
-	assert.Equal(t, 2, len(queueQuery))
-	assert.Equal(t, 20, queueQuery["genre1"])
+func TestNewQueueQuery(t *testing.T) {
+
+	// TODO: check for all other wise intersection
+
+	// test Empty
+	emptyQuery := NewQueueQuery()
+	assert.NotNil(t, emptyQuery)
+
+	queueQuery := NewQueueQueryFromMap(qCountConfig)
+
+	assert.Equal(t, 1, len(queueQuery[QBreaking]))
+	assert.Equal(t, 2, len(queueQuery[QGenre]))
+
+	assert.Equal(t, 10, queueQuery[QBreaking]["breaking"])
+	assert.Equal(t, 20, queueQuery[QGenre]["genre-1"], "genre-1 count should be 20")
+	assert.Equal(t, 5, queueQuery[QGenre]["genre-2"], "genre-2 count should be 5")
 }
 
 func TestInstanceOfSourceQuery(t *testing.T) {
 
-	queueQuery := NewQueueQuery().
-		Add("breaking", 10).
-		Add("genre1", 30).
-		Add("genre2", 5)
+	queueQuery := NewQueueQueryFromMap(qCountConfig)
+	queueQuery.Add(QGenre, "genre-3", 5)
 
 	sourceQuery := NewSourceQueryBuilder().
 		Select(queueQuery).
 		From(util.HINDI).
-		Filter(MatchAny("Genre1", "Genre100")).
-		Filter(MatchAll("IG3", "IG4")).
-		Filter(MatchNone("Racy")).
+		Filter(GENRE, MatchAny("Genre1", "Genre100")).
+		Filter(GENRE, MatchAll("Genre3", "Genre4", "Genre5")).
+		Filter(IG, MatchAll("IG3", "IG4")).
+		Filter(NSFW, MatchNone("Racy")).
+		Filter(NSFW, MatchNone("Adult")).
 		Build()
 
 	assert.NotNil(t, sourceQuery)
+
+	genreAnyFilters := sourceQuery.orFilters[GENRE]
+	assert.Equal(t, 1, len(genreAnyFilters))
+	assert.Equal(t, 2, len(genreAnyFilters[0].Fields()))
+
+	igAllFilters := sourceQuery.andFilters[IG]
+	assert.Equal(t, 1, len(igAllFilters))
+	assert.Equal(t, 2, len(igAllFilters[0].Fields()))
+
+	genreAllFilters := sourceQuery.andFilters[GENRE]
+	assert.Equal(t, 1, len(genreAllFilters))
+	assert.Equal(t, 3, len(genreAllFilters[0].Fields()))
+
+	notNsfwFilters := sourceQuery.notFilters[NSFW]
+	assert.Equal(t, 2, len(notNsfwFilters))
+	assert.Equal(t, 1, len(notNsfwFilters[1].Fields()))
 
 }
